127f8556a87a7428578d6e9f8b6bd7769727a0c9
mrawesomeac@gmail.com <mrawesomeac@gmail.com>
2024-01-04 12:12:11 -0500

    Made some leaps and bounds

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,8 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -18,26 +18,81 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     }
 
     @Override
-    public List<Category> getAllCategories()
+    public List<String> getAllCategories()
     {
-        // get all categories
-        return null;
+        List<String> category = new ArrayList<>();
+      ;
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM category WHERE name = ?")) {
+
+
+                preparedStatement.setString(1, String.valueOf(category));
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    int categoryId = resultSet.getInt("category_id");
+                    String description = resultSet.getString("description");
+                    String name = resultSet.getString("name");
+                    String fullCategoryInfo = categoryId + " " + name + " " + description;
+                    category.add(fullCategoryInfo);
+
+                    for (String categoryI: category )
+                    {
+                        System.out.println(categoryI);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return category;
+    }
+
+
+    @Override
+    public int getById(int categoryId) {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM category WHERE category_id = ?")) {
+                preparedStatement.setInt(1, (categoryId));
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return mapRow(resultSet).getCategoryId();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return categoryId;
     }
 
     @Override
-    public Category getById(int categoryId)
-    {
-        // get category by id
+    public Category create(Category category) {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO categories (name, description, category_id) Values (?, ?, ?)")) {
+
+                preparedStatement.setString(1, category.getName());
+                preparedStatement.setString(2, category.getDescription());
+                preparedStatement.setInt(3, (category.getCategoryId()));
+                int affectedRows = preparedStatement.executeUpdate();
+
+                if (affectedRows > 0){
+                    try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()){
+                        if (generatedKeys.next()){
+
+                        }
+                    }
+                }
+        }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
         return null;
     }
-
-    @Override
-    public Category create(Category category)
-    {
-        // create a new category
-        return null;
-    }
-
     @Override
     public void update(int categoryId, Category category)
     {
@@ -66,4 +121,6 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         return category;
     }
 
-}
+} catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
