76dab5ef81f4ac55ac4080607a93c9acfb8dfd4c
Christian Crites <ccrites98@gmail.com>
2024-01-04 10:14:38 -0500

    fixedthecontroler

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,9 +1,7 @@
 package org.yearup.controllers;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -11,66 +9,45 @@ import org.yearup.models.Product;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("/categories")
 @CrossOrigin(origins = "http://localhost:8080")
+public class CategoriesController {
+    private final CategoryDao categoryDao;
+    private final ProductDao productDao;
 
-public class CategoriesController
-{
-    private CategoryDao categoryDao;
-    private ProductDao productDao;
-
-
-    // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
-    public static List<Category> getAllCategories(String CategoryName) {
-        List<String> category = new ArrayList<>();
-
-        // find and return all categories
-        return null;
-
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
     }
-
-    // add the appropriate annotation for a get action
-    public static Category getById(@PathVariable int id)
-    {
-        // get the category by id
-        return null;
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryDao.getAllCategories();
     }
-
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        // get a list of product by categoryId
-        return null;
+    @GetMapping("/3")
+    public Category getById(@PathVariable int id) {
+        return categoryDao.getById(id);
     }
-
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
-    public Category addCategory(@RequestBody Category category)
-    {
-        // insert the category
-        return null;
+    @GetMapping("/3/products")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        return productDao.getProductsByCategoryId(categoryId);
     }
-
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
-        // update the category by id
+    @PostMapping
+    @Secured("ROLE_ADMIN")
+    public Category addCategory(@RequestBody Category category) {
+        return categoryDao.create(category);
     }
-
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
-    public void deleteCategory(@PathVariable int id)
-    {
-        // delete the category by id
+    @PutMapping("/3")
+    @Secured("ROLE_ADMIN")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        categoryDao.update(id, category);
+    }
+    @DeleteMapping("/3")
+    @Secured("ROLE_ADMIN")
+    public void deleteCategory(@PathVariable int id) {
+        categoryDao.delete(id);
     }
 }
+
+
